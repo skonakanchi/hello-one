4396d1c51e8b261e1d9508d693f5825254e26c00
Srihari Konakanchi <contactsrihari@gmail.com>
2014-08-25 19:20:52 +0100

    Revert "HelloOne,java changed"
    This reverts commit 9c0ebd64d61c0df314807ef74154b36195b7e0ef.

--- src/com/git/HelloOne.java
+++ src/com/git/HelloOne.java
@@ -16,7 +16,7 @@ public class HelloOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("second git project - changed");
+        System.out.println("second git project");
     }
     
 }
